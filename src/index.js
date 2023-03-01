0a8e2d9bd6568c58b947a0f7b6faa45dc8bde533
abdullox0900 <abdullox0900@gmail.com>
2023-03-01 16:46:40 +0500

    [Add]: forst commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: Open-Sans, Helvetica, Sans-Serif;
+    }
+`;
+
+export default GlobalStyle;
